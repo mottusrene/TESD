c541ae55f6ba5c7a9a02e067d68a3bd7a1dc78ca
René Mõttus <mottus.rene@gmail.com>
2021-09-06 12:41:53 +0100

    Publish files

---
+++ R/TESD.R
@@ -0,0 +1,74 @@
+TESD <- function(x,y, plot=TRUE, Xlab="X",Ylab="Y", plot.expected.TESD=FALSE){
+
+  cut3 <- function(x) cut(x,quantile(x,c(0,1/3,2/3,1)),c("Low","Medium","High"), include.lowest=TRUE)
+
+  tabs <- data.frame(y = cut3(y), x = cut3(x))
+
+  crosstabs <- apply(prop.table(table(tabs)),2,function(x) x/sum(x))
+
+  crosstabs <- crosstabs[c("High","Medium","Low"),c("Low","Medium","High")]
+
+  percents <- matrix(paste(round((crosstabs * 100),1),"%", sep=""), ncol=3)
+
+
+  ## Expected TESD for the correlation if the variables were normally distributed
+
+  tx <- rnorm(10^6)
+  ty <- cor(x,y)* tx + sqrt(1- cor(x,y)^2)*rnorm(length(tx))
+  ttabs <- data.frame(y = cut3(ty), x = cut3(tx))
+  tcrosstabs <- apply(prop.table(table(ttabs)),2,function(x) x/sum(x))
+  tcrosstabs <- tcrosstabs[c("High","Medium","Low"),c("Low","Medium","High")]
+  tpercents <- matrix(paste(round((tcrosstabs * 100),1),"%", sep=""), ncol=3)
+
+  if(plot.expected.TESD)
+    percents <- matrix(paste(percents, tpercents, sep = "\nE: "), ncol=3)
+  if(plot){
+    color <- as.factor(apply(tabs, 1, paste, collapse=""))
+
+    plot(x,y, cex=.5, ,
+         xlab=Xlab, ylab=Ylab,
+         col = c("olivedrab1","orange", "lightblue", "green", "red", "dodgerblue2", "darkgreen","brown","midnightblue")[color] )
+
+    xq <- quantile(x, c(1/3, 2/3))
+    yq <- quantile(y, c(1/3, 2/3))
+
+    lines(rep(xq[1],2),c(min(y) - abs(min(y)),max(y) + abs(max(y))), col="grey50")
+    lines(rep(xq[2],2),c(min(y) - abs(min(y)),max(y) + abs(max(y))), col="grey50")
+
+    lines(c(min(x) - abs(min(x)),max(x) + abs(max(x))), rep(yq[1],2), col="grey50")
+    lines(c(min(x) - abs(min(x)),max(x) + abs(max(x))), rep(yq[2],2), col="grey50")
+
+    text((xq[1] + min(x))/2, (yq[2] + max(y))/2, percents[1,1], cex=1)
+    text((xq[1] + min(x))/2, mean(y), percents[2,1], cex=1)
+    text((xq[1] + min(x))/2, (yq[1] + min(y))/2, percents[3,1], cex=1)
+
+    text(mean(x), (yq[2] + max(y))/2, percents[1,2], cex=1)
+    text(mean(x), mean(y), percents[2,2], cex=1)
+    text(mean(x), (yq[1] + min(y))/2, percents[3,2], cex=1)
+
+    text((xq[2] + max(x))/2, (yq[2] + max(y))/2, percents[1,3], cex=1)
+    text((xq[2] + max(x))/2, mean(y), percents[2,3], cex=1)
+    text((xq[2] + max(x))/2, (yq[1] + min(y))/2, percents[3,3], cex=1)
+
+  }
+
+  warning(c("\n\n",
+            paste("Interpret TESD from supplied data as:
+          Proportions of", Ylab, "if", Xlab," is either low, medium or high, respectively"), "\n"))
+
+  if(min(rowSums(crosstabs)) < .95 | max(rowSums(crosstabs)) > 1.05)
+    warning("\n\nBut TESD groups have unexpected sizes (see univariate and multivariate proportions), so be careful!
+            These variables may not be suitable for interpreting TESD from supplied data.
+            This is probably beause the variables have a limited range of values, which makes group sizes unequal.
+            Interpret the estimated TESD instead (assuming variables should be normally distributed). It is printed.
+            You can also switch it on by plot.expected.TESD = TRUE.\n")
+
+  list(
+    TESD.table.from.supplied.data = crosstabs,
+    expected.TESD = tcrosstabs,
+    univariate.x.proportions = prop.table(table(tabs$x)),
+    univariate.y.propoprtions = prop.table(table(tabs$y)),
+    cross.tabulations = table(tabs),
+    multivariate.y.proportions = rowSums(crosstabs)
+  )
+}
---
+++ R/TESD.expected.R
@@ -0,0 +1,54 @@
+TESD.expected <- function(r, distribution = c("normal", "uniform", "skewed"), n = 10^3, plot=TRUE){
+
+  cut3 <- function(x) cut(x,quantile(x,c(0,1/3,2/3,1)),c("Low","Medium","High"), include.lowest=TRUE)
+
+  if(distribution == "normal"){
+    x <- rnorm(n)
+    y <- r * x + sqrt(1 - r^2)*rnorm(n)
+  }
+
+  if(distribution == "uniform"){
+    x <- runif(n, -1, 1)
+    y <- r * x + sqrt(1 - r^2)*runif(n,-1,1)
+  }
+
+  if(distribution == "skewed"){
+    x <- rbeta(n, 5, 1)
+    y <- r * x + sqrt(1 - r^2)*rbeta(n, 5, 1)
+  }
+
+  tabs <- data.frame(y = cut3(y), x = cut3(x))
+  crosstabs <- apply(prop.table(table(tabs)),2,function(x) x/sum(x))
+  crosstabs <- crosstabs[c("High","Medium","Low"),c("Low","Medium","High")]
+  percents <- matrix(paste(round((crosstabs * 100),1),"%", sep=""), ncol=3)
+  if(plot){
+    color = as.factor(apply(tabs, 1, paste, collapse=""))
+
+    plot(x,y, cex=.5, ,
+         xlab="X", ylab="Y",
+         col = c("olivedrab1","orange", "lightblue", "green", "red", "dodgerblue2", "darkgreen","brown","midnightblue")[color] )
+
+    xq <- quantile(x, c(1/3, 2/3))
+    yq <- quantile(y, c(1/3, 2/3))
+
+    lines(rep(xq[1],2),c(min(y) - abs(min(y)),max(y) + abs(max(y))), col="grey50")
+    lines(rep(xq[2],2),c(min(y) - abs(min(y)),max(y) + abs(max(y))), col="grey50")
+
+    lines(c(min(x) - abs(min(x)),max(x) + abs(max(x))), rep(yq[1],2), col="grey50")
+    lines(c(min(x) - abs(min(x)),max(x) + abs(max(x))), rep(yq[2],2), col="grey50")
+
+    text((xq[1] + min(x))/2, (yq[2] + max(y))/2, percents[1,1], cex=1)
+    text((xq[1] + min(x))/2, mean(y), percents[2,1], cex=1)
+    text((xq[1] + min(x))/2, (yq[1] + min(y))/2, percents[3,1], cex=1)
+
+    text(mean(x), (yq[2] + max(y))/2, percents[1,2], cex=1)
+    text(mean(x), mean(y), percents[2,2], cex=1)
+    text(mean(x), (yq[1] + min(y))/2, percents[3,2], cex=1)
+
+    text((xq[2] + max(x))/2, (yq[2] + max(y))/2, percents[1,3], cex=1)
+    text((xq[2] + max(x))/2, mean(y), percents[2,3], cex=1)
+    text((xq[2] + max(x))/2, (yq[1] + min(y))/2, percents[3,3], cex=1)
+  }
+  crosstabs
+
+}
